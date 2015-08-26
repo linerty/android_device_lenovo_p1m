0d12bfb182650ad9eabd1a6deb7d3d2e0d1ac926
Alex Mirniy <linerty@gmail.com>
2015-08-26 21:31:59 +0500

    First load

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/p1m.mk \
+	$(LOCAL_DIR)/omni_p1m.mk
---
+++ omni_p1m.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/lenovo/p1m/p1m.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p1m
+PRODUCT_NAME := omni_p1m
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := aio_row
+PRODUCT_MANUFACTURER := lenovo
